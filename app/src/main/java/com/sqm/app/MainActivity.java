248f4fc39d926e4ce990df152b00fe7e7d0c8cb8
chenlu <413162035@qq.com>
2021-12-08 17:01:02 +0800

    update groupId = 'com.sqcat.cn'

--- app/src/main/java/com/sqm/app/MainActivity.java
+++ app/src/main/java/com/sqm/app/MainActivity.java
@@ -4,11 +4,14 @@ import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.sqm.advert_helper.adv.CommonAdvertLoadHelper;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+//        CommonAdvertLoadHelper.shoNewExpressAdv();
     }
 }
